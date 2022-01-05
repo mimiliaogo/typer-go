875573dcdfbc3bfd805d9188bffea14a418276d7
Stevn Wang <engine210@gmail.com>
2022-01-05 19:15:30 +0800

    move run_server.go to right place

--- server/run_server.go
+++ server/run_server.go
@@ -1,14 +1,9 @@
 package main
+
 import (
 	"github.com/kanopeld/go-socket"
-	// "github.com/rivo/tview"
 	"github.com/shilangyu/typer-go/game"
 	"github.com/shilangyu/typer-go/utils"
-	// "github.com/gdamore/tcell"
-	// "log"
-	// "strconv"
-	// "fmt"
-	// "time"
 )
 
 type setup struct {
@@ -20,12 +15,10 @@ type setup struct {
 }
 
 func main() {
-	
 	IP := "127.0.0.1"
 	setup := setup{IP, "", "9001", true, nil, nil}
 	var err error
 	setup.Server, err = game.NewServer(setup.Port)
 	utils.Check(err)
 	setup.Server.Stop()
-	
 }
